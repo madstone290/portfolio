21151cf71f46e80b04fdbe64e3eacf5dc2d8160c
madstone290 <madstone290@gmail.com>
2023-02-07 17:57:15 +0900

    feat 슬라이더 확대기능 추가

--- assets/js/base.js
+++ assets/js/base.js
@@ -26,70 +26,115 @@ new ResizeObserver(outputsize).observe(header);
 
 // 모달
 const modal = document.querySelector('.modal');
-const modalClose = document.querySelector(".modal__close");
-modal.addEventListener("click", (evt) => {
-    if (evt.target == modal)
-        modal.style.display = "none";
-});
-
-modalClose.addEventListener("click", (evt) => {
+const modalBody = document.querySelector(".modal__body");
+modal.isOpen = () => {
+    return modal.style.display == "flex";
+};
+modal.closeCallback = function () { };
+modal.open = () => {
+    modal.style.display = "flex";
+};
+modal.close = function () {
     modal.style.display = "none";
+    if (modal.closeCallback)
+        modal.closeCallback();
+};
+modal.setContent = function (content) {
+    modalBody.innerHTML = "";
+    modalBody.appendChild(content);
+};
+
+modal.addEventListener("click", (evt) => {
+    if (evt.target == modal) {
+        modal.close();
+    }
 });
 
-// 이미지 클릭시 확대 다이얼로그 팝업
-const projectImgs = document.querySelectorAll(".slider__img");
-const modalImg = document.querySelector(".modal__img");
-for (const img of projectImgs) {
-    img.addEventListener("click", (evt) => {
-        modalImg.src = img.src;
-        modal.style.display = "flex";
-    });
-}
+const modalClose = document.querySelector(".modal__close");
+modalClose.addEventListener("click", (evt) => {
+    modal.close();
+});
 
+// 이미지 슬라이더
+const sliderManager = {
+    currentItemIndexMap: new Map(), // 슬라이더의 현재 아이템 인덱스. key: 슬라이더 인덱스, value: 아이템인덱스
+    sliderItemsMap: new Map(), // 슬라이더의 아이템. key: 슬라이더 인덱스, value: 아이템목록
+    scaledSliderItemsMap: new Map(), // 현재 팝업된 슬라이더의 아이템. key: 슬라이더 인덱스, value: 아이템목록
+    sliders: document.querySelectorAll(".slider"),
+    init: function () {
+        for (let index = 0; index < this.sliders.length; index++) {
+            // 기본 인덱스 0 설정
+            this.currentItemIndexMap.set(index, 0);
 
-// 이미지 슬라이드
-const currentItemIndexMap = new Map(); // key: 슬라이더인덱스, value: 현재 선택된 아이템 인덱스
-const sliderItemsMap = new Map(); // key: 슬라이더 인덱스, value: 슬라이더에 포함된 아이템목록
-const sliders = document.querySelectorAll(".slider");
+            // 아이템 배열 조회
+            const items = this.sliders[index].querySelectorAll(".slider__item");
+            this.sliderItemsMap.set(index, items);
+        }
 
-for(let index = 0; index < sliders.length ; index++){
-    
-    // 기본 인덱스 0 설정
-    currentItemIndexMap.set(index, 0);
-    
-    // 아이템 배열 조회
-    const items = sliders[index].querySelectorAll(".slider__item");
-    sliderItemsMap.set(index, items);
-}
+        for (let index = 0; index < this.sliders.length; index++) {
+            this.show(index, this.currentItemIndexMap.get(index));
+        }
+    },
+    move: function (sliderIndex, number) {
+        this.show(sliderIndex, this.currentItemIndexMap.get(sliderIndex) + number);
+    },
+    show: function (sliderIndex, itemIndex) {
+        // 아이템 인덱스 유효성 검사
+        const itemsCount =this. sliderItemsMap.get(sliderIndex).length;
+        const validIndex = (itemIndex + itemsCount) % itemsCount;
 
-// 현재 인덱스에 해당하는 아이템 보이기
-for(let index = 0; index < sliders.length; index++){
-    showSlider(index, currentItemIndexMap.get(index));
-}
+        // 아이템 표시
+        this.showInternal(sliderIndex, validIndex);
 
-function moveSlider(sliderIndex, number) {
-    showSlider(sliderIndex, currentItemIndexMap.get(sliderIndex) + number);
-}
+        // 현재 아이템 인덱스 갱신
+        this.currentItemIndexMap.set(sliderIndex, validIndex);
+    },
+    showInternal: function (sliderIndex, targetItemIndex) {
+        const sliderItems = this.sliderItemsMap.get(sliderIndex);
+        for (let itemIndex = 0; itemIndex < sliderItems.length; itemIndex++) {
+            if (itemIndex == targetItemIndex)
+                sliderItems[itemIndex].style.display = "block"
+            else
+                sliderItems[itemIndex].style.display = "none"
+        }
 
-function showSlider(sliderIndex, itemIndex) {
-    // 아이템 인덱스 유효성 검사
-    const itemsCount = sliderItemsMap.get(sliderIndex).length;
-    const validIndex = (itemIndex + itemsCount) % itemsCount;
-
-    // 아이템 표시
-    showSliderInternal(sliderIndex, validIndex);
-
-    // 현재 아이템 인덱스 갱신
-    currentItemIndexMap.set(sliderIndex ,validIndex);
-}
-
-function showSliderInternal(sliderIndex, targetItemIndex) {
-    const sliderItems = sliderItemsMap.get(sliderIndex);
-    
-    for (let itemIndex = 0; itemIndex < sliderItems.length; itemIndex++) {
-        if (itemIndex == targetItemIndex)
-            sliderItems[itemIndex].style.display = "block"
-        else
-            sliderItems[itemIndex].style.display = "none"
+        const scaledSliderItems = this.scaledSliderItemsMap.get(sliderIndex);
+        if(!scaledSliderItems)
+            return;
+        for (let itemIndex = 0; itemIndex < scaledSliderItems.length; itemIndex++) {
+            if (itemIndex == targetItemIndex)
+                scaledSliderItems[itemIndex].style.display = "block"
+            else
+                scaledSliderItems[itemIndex].style.display = "none"
+        }
     }
+};
+sliderManager.init();
+
+function moveSlider(groupIndex, itemIndex){
+    sliderManager.move(groupIndex, itemIndex);
+}
+
+// 이미지 슬라이드 클릭시 모달 팝업
+for (let index = 0; index < sliderManager.sliders.length; index++) {
+    const slider = sliderManager.sliders[index];
+    slider.addEventListener("click", (evt) => {
+        // 현재 모달상태가 아니고 img태그에서 클릭이 발생한 경우에만 모달 팝업
+        if (modal.isOpen())
+            return;
+        if (evt.target.tagName.toLowerCase() != "img")
+            return;
+
+        // 현재 슬라이더를 모달창에 복사 후 팝업
+        const copiedSlider = slider.cloneNode(true);
+        modal.setContent(copiedSlider);
+        modal.closeCallback = () => {
+            // callback 적용
+        };
+        modal.open();
+
+        // 슬라이더 동작을 위해 복사된 슬라이더의 아이템 설정
+        const items = copiedSlider.querySelectorAll(".slider__item");
+        sliderManager.scaledSliderItemsMap.set(index, items);
+    });
 }
