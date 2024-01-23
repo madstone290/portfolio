ff3267ed14f7c037f79cf3bb59d920d3c9943427
madstone290 <madstone290@gmail.com>
2024-01-23 09:06:56 +0900

    Fix 내용 업데이트
    React

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,18 +1,3 @@
-// 커리어 프로젝트 열기/닫기 버튼 핸들러
-const btn = document.getElementById("btn-careers-projects-1");
-const list = document.getElementById("careers-projects-1");
-btn.addEventListener("click", (ev) => {
-    if (btn.textContent == "열기") {
-        list.hidden = false;
-        btn.textContent = "닫기";
-    }
-    else {
-
-        list.hidden = true;
-        btn.textContent = "열기";
-    }
-});
-
 // 헤더 사이즈 변경시 패딩 설정 핸들러
 const root = document.querySelector(':root');
 const rootStyle = getComputedStyle(root);
