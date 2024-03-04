5f99d5f979e99319bfc5befea6a361eff5851969
madstone290 <madstone290@gmail.com>
2024-03-04 17:52:04 +0900

    Chore 주석 추가

--- portfolio-react/src/components/ripple/Ripple.tsx
+++ portfolio-react/src/components/ripple/Ripple.tsx
@@ -6,7 +6,12 @@ interface RippleProps {
     clientY: number;
 }
 
-
+/**
+ * 컴포넌트 렌더링시에 물결 효과가 생성되는 컴포넌트.
+ * 애니메이션이 종료될 때 컴포넌트를 제거해줘야 한다.
+ * @param props 
+ * @returns 
+ */
 export default function Ripple(props: RippleProps) {
     const { clientX, clientY } = props;
     return (
