aac42f8a5b4ba9ac16b58e0a1171cbc1cc0f4d1a
madstone290 <madstone290@gmail.com>
2024-03-06 16:42:36 +0900

    Fix 리플 크기 변경

--- portfolio-react/src/components/ripple/Ripple.tsx
+++ portfolio-react/src/components/ripple/Ripple.tsx
@@ -16,8 +16,8 @@ export default function Ripple(props: RippleProps) {
     const { clientX, clientY } = props;
     return (
         <div className={css.ripple} style={{
-            left: (clientX - 50) + 'px',
-            top: (clientY - 50) + 'px'
+            left: (clientX - 40) + 'px',
+            top: (clientY - 40) + 'px'
         }}>
             <div className={css.line} />
             <div className={css.line} />
